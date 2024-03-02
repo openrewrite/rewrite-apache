86e54f124bef7da226b560f048b4a97460e927f4
Tim te Beek <tim@moderne.io>
2024-03-02 14:28:32 +0100

    Complete import of Apache project recipes
    Fixes https://github.com/openrewrite/rewrite-apache/issues/1

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("org.openrewrite.build.recipe-library") version "latest.release"
+}
+
+group = "org.openrewrite.recipe"
+description = "Apache Migration"
+
+recipeDependencies {
+    parserClasspath("org.apache.httpcomponents.core5:httpcore5:5.1.+")
+    parserClasspath("org.apache.httpcomponents.client5:httpclient5:5.1.+")
+}
+
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
+dependencies {
+    implementation(platform("org.openrewrite:rewrite-bom:$rewriteVersion"))
+    implementation("org.openrewrite:rewrite-java")
+    implementation("org.openrewrite.recipe:rewrite-java-dependencies:$rewriteVersion")
+    implementation("org.openrewrite.recipe:rewrite-static-analysis:$rewriteVersion")
+    implementation("org.openrewrite:rewrite-templating:$rewriteVersion")
+
+    annotationProcessor("org.openrewrite:rewrite-templating:$rewriteVersion")
+    compileOnly("com.google.errorprone:error_prone_core:2.19.1:with-dependencies") {
+        exclude("com.google.auto.service", "auto-service-annotations")
+    }
+
+    implementation("commons-io:commons-io:2.+")
+    implementation("org.apache.commons:commons-lang3:3.+")
+    implementation("org.apache.maven.shared:maven-shared-utils:3.+")
+    implementation("org.codehaus.plexus:plexus-utils:3.+")
+
+    testImplementation("org.openrewrite:rewrite-java-17")
+    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.openrewrite:rewrite-maven")
+
+    testImplementation("commons-codec:commons-codec:1.+")
+
+    testRuntimeOnly("org.apache.httpcomponents:httpclient:4.5.14")
+    testRuntimeOnly("org.apache.httpcomponents.client5:httpclient5:5.1.+")
+
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "rewrite-apache"
+
+plugins {
+    id("com.gradle.enterprise") version "latest.release"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.12.1"
+}
+
+gradleEnterprise {
+    val isCiServer = System.getenv("CI")?.equals("true") ?: false
+    server = "https://ge.openrewrite.org/"
+
+    buildCache {
+        remote(gradleEnterprise.buildCache) {
+            isEnabled = true
+            val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
+            isPush = isCiServer && !accessKey.isNullOrBlank()
+        }
+    }
+
+    buildScan {
+        capture {
+            isTaskInputFiles = true
+        }
+
+        isUploadInBackground = !isCiServer
+
+        publishAlways()
+        this as com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+        publishIfAuthenticated()
+    }
+}
