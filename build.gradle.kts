b796ee34c8866a635ce0bfaa0979be22bcfa0e1d
sullis <github@seansullivan.com>
2024-03-28 07:24:34 -0700

    add recipe for Apache Commons Math 3.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,9 @@ dependencies {
     testImplementation("commons-collections:commons-collections:3.2.2")
     testImplementation("org.apache.commons:commons-collections4:4.4")
 
+    testImplementation("org.apache.commons:commons-math:2.2")
+    testImplementation("org.apache.commons:commons-math3:3.+")
+
     testImplementation("commons-lang:commons-lang:2.6")
     testImplementation("org.apache.commons:commons-lang3:3.+")
 
