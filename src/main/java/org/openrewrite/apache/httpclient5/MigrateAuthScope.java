8317221a8eb22aa6285400f603c1e74bf5355a0d
Adriano Machado <60320+ammachado@users.noreply.github.com>
2024-04-13 14:01:41 -0400

    Adding new migrations for HTTP5 Client (#14)
    * Adding new migrations for HTTP5 Client
    
    * Adding new migrations for HTTP5 Client
    
    * Update src/test/java/org/openrewrite/apache/httpclient5/MigrateAuthScopeTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/main/java/org/openrewrite/apache/httpclient5/MigrateAuthScope.java
    
    ---------
    
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/main/java/org/openrewrite/apache/httpclient5/MigrateAuthScope.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.apache.httpclient5;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.TypeUtils;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class MigrateAuthScope extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Replaces `AuthScope.ANY`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replace removed constant `org.apache.http.auth.AuthScope.AuthScope.ANY` with `new org.apache.hc.client5.http.auth.AuthScope(null, -1)`";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+                new UsesType<>("org.apache.hc.client5.http.auth.AuthScope", false),
+                new JavaVisitor<ExecutionContext>() {
+                    @Override
+                    public J visitFieldAccess(J.FieldAccess fieldAccess, ExecutionContext ctx) {
+                        J.FieldAccess f = (J.FieldAccess) super.visitFieldAccess(fieldAccess, ctx);
+                        // Type was already relocated by previous recipe
+                        if ("ANY".equals(f.getSimpleName()) && TypeUtils.isOfClassType(f.getTarget().getType(), "org.apache.hc.client5.http.auth.AuthScope")) {
+                            maybeAddImport("org.apache.hc.client5.http.auth.AuthScope");
+                            return JavaTemplate.builder("new AuthScope(null, -1)")
+                                    .imports("org.apache.hc.client5.http.auth.AuthScope")
+                                    .javaParser(JavaParser.fromJavaVersion().classpathFromResources(ctx, "httpcore5"))
+                                    .build()
+                                    .apply(updateCursor(f), f.getCoordinates().replace());
+                        }
+                        return f;
+                    }
+                });
+    }
+}
