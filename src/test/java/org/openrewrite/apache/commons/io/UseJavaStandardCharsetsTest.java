4b4c169cec7997c00f536c4b34a04f810f933af7
Tim te Beek <timtebeek@gmail.com>
2024-04-14 20:14:15 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.OpenRewriteBestPractices?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/apache/commons/io/UseJavaStandardCharsetsTest.java
+++ src/test/java/org/openrewrite/apache/commons/io/UseJavaStandardCharsetsTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.apache.commons.io;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -30,6 +31,7 @@ class UseJavaStandardCharsetsTest implements RewriteTest {
           .parser(JavaParser.fromJavaVersion().classpath("commons-io"));
     }
 
+    @DocumentExample
     @Test
     @SuppressWarnings("deprecation")
     void toStandardCharsets() {
