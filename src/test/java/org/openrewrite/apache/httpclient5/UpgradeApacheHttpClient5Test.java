8affb54306bb42968d754a36b248ecebb5229842
SiBorea <108953913+SiBorea@users.noreply.github.com>
2024-11-28 19:44:08 +0800

    Migrate UsernamePasswordCredentials (#44)
    * Migrate UsernamePasswordCredentials
    
    * Minor polish
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- src/test/java/org/openrewrite/apache/httpclient5/UpgradeApacheHttpClient5Test.java
+++ src/test/java/org/openrewrite/apache/httpclient5/UpgradeApacheHttpClient5Test.java
@@ -49,39 +49,40 @@ class UpgradeApacheHttpClient5Test implements RewriteTest {
         rewriteRun(
           //language=xml
           pomXml(
-                """
-            <project>
-                <modelVersion>4.0.0</modelVersion>
-                <groupId>org.example</groupId>
-                <artifactId>example</artifactId>
-                <version>1.0.0</version>
-                <dependencies>
-                    <dependency>
-                        <groupId>org.apache.httpcomponents</groupId>
-                        <artifactId>httpclient</artifactId>
-                        <version>4.5.14</version>
-                    </dependency>
-                </dependencies>
-            </project>
-            """, spec -> spec.after(pom -> {
-              Matcher version = Pattern.compile("5\\.4\\.\\d+").matcher(pom);
-              assertThat(version.find()).describedAs("Expected 5.4.x in %s", pom).isTrue();
-              return """
-                <project>
-                    <modelVersion>4.0.0</modelVersion>
-                    <groupId>org.example</groupId>
-                    <artifactId>example</artifactId>
-                    <version>1.0.0</version>
-                    <dependencies>
-                        <dependency>
-                            <groupId>org.apache.httpcomponents.client5</groupId>
-                            <artifactId>httpclient5</artifactId>
-                            <version>%s</version>
-                        </dependency>
-                    </dependencies>
-                </project>
-                """.formatted(version.group(0));
-          })));
+            """
+              <project>
+                  <modelVersion>4.0.0</modelVersion>
+                  <groupId>org.example</groupId>
+                  <artifactId>example</artifactId>
+                  <version>1.0.0</version>
+                  <dependencies>
+                      <dependency>
+                          <groupId>org.apache.httpcomponents</groupId>
+                          <artifactId>httpclient</artifactId>
+                          <version>4.5.14</version>
+                      </dependency>
+                  </dependencies>
+              </project>
+              """,
+            spec -> spec.after(pom -> {
+                Matcher version = Pattern.compile("5\\.4\\.\\d+").matcher(pom);
+                assertThat(version.find()).describedAs("Expected 5.4.x in %s", pom).isTrue();
+                return """
+                  <project>
+                      <modelVersion>4.0.0</modelVersion>
+                      <groupId>org.example</groupId>
+                      <artifactId>example</artifactId>
+                      <version>1.0.0</version>
+                      <dependencies>
+                          <dependency>
+                              <groupId>org.apache.httpcomponents.client5</groupId>
+                              <artifactId>httpclient5</artifactId>
+                              <version>%s</version>
+                          </dependency>
+                      </dependencies>
+                  </project>
+                  """.formatted(version.group(0));
+            })));
     }
 
     @DocumentExample
@@ -90,31 +91,32 @@ class UpgradeApacheHttpClient5Test implements RewriteTest {
         rewriteRun(
           //language=java
           java(
-                """
-            import org.apache.http.HttpEntity;
-            import org.apache.http.client.methods.HttpGet;
-            import org.apache.http.client.methods.HttpUriRequest;
-            import org.apache.http.util.EntityUtils;
+            """
+              import org.apache.http.HttpEntity;
+              import org.apache.http.client.methods.HttpGet;
+              import org.apache.http.client.methods.HttpUriRequest;
+              import org.apache.http.util.EntityUtils;
 
-            class A {
-                void method(HttpEntity entity, String urlStr) throws Exception {
-                    HttpUriRequest getRequest = new HttpGet(urlStr);
-                    EntityUtils.consume(entity);
-                }
-            }
-            """, """
-            import org.apache.hc.core5.http.io.entity.EntityUtils;
-            import org.apache.hc.core5.http.HttpEntity;
-            import org.apache.hc.client5.http.classic.methods.HttpGet;
-            import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+              class A {
+                  void method(HttpEntity entity, String urlStr) throws Exception {
+                      HttpUriRequest getRequest = new HttpGet(urlStr);
+                      EntityUtils.consume(entity);
+                  }
+              }
+              """, """
+              import org.apache.hc.core5.http.io.entity.EntityUtils;
+              import org.apache.hc.core5.http.HttpEntity;
+              import org.apache.hc.client5.http.classic.methods.HttpGet;
+              import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
 
-            class A {
-                void method(HttpEntity entity, String urlStr) throws Exception {
-                    HttpUriRequest getRequest = new HttpGet(urlStr);
-                    EntityUtils.consume(entity);
-                }
-            }
-            """)
+              class A {
+                  void method(HttpEntity entity, String urlStr) throws Exception {
+                      HttpUriRequest getRequest = new HttpGet(urlStr);
+                      EntityUtils.consume(entity);
+                  }
+              }
+              """
+          )
         );
     }
 
@@ -123,45 +125,47 @@ class UpgradeApacheHttpClient5Test implements RewriteTest {
         rewriteRun(
           //language=java
           java(
-                """
-            import org.apache.http.client.config.RequestConfig;
-            import org.apache.http.config.SocketConfig;
+            """
+              import org.apache.http.client.config.RequestConfig;
+              import org.apache.http.config.SocketConfig;
 
-            class A {
-                void method() {
-                    int connectTimeout = 500;
-                    RequestConfig.custom()
-                        .setConnectionRequestTimeout(300)
-                        .setConnectTimeout(connectTimeout)
-                        .setSocketTimeout(connectTimeout * 10);
+              class A {
+                  void method() {
+                      int connectTimeout = 500;
+                      RequestConfig.custom()
+                          .setConnectionRequestTimeout(300)
+                          .setConnectTimeout(connectTimeout)
+                          .setSocketTimeout(connectTimeout * 10);
 
-                    long linger = 420;
-                    SocketConfig.custom()
-                        .setSoTimeout(1000)
-                        .setSoLinger((int) linger);
-                }
-            }
-            """, """
-            import org.apache.hc.client5.http.config.RequestConfig;
-            import org.apache.hc.core5.http.io.SocketConfig;
+                      long linger = 420;
+                      SocketConfig.custom()
+                          .setSoTimeout(1000)
+                          .setSoLinger((int) linger);
+                  }
+              }
+              """,
+            """
+              import org.apache.hc.client5.http.config.RequestConfig;
+              import org.apache.hc.core5.http.io.SocketConfig;
 
-            import java.util.concurrent.TimeUnit;
+              import java.util.concurrent.TimeUnit;
 
-            class A {
-                void method() {
-                    int connectTimeout = 500;
-                    RequestConfig.custom()
-                        .setConnectionRequestTimeout(300, TimeUnit.MILLISECONDS)
-                        .setConnectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
-                        .setResponseTimeout(connectTimeout * 10, TimeUnit.MILLISECONDS);
+              class A {
+                  void method() {
+                      int connectTimeout = 500;
+                      RequestConfig.custom()
+                          .setConnectionRequestTimeout(300, TimeUnit.MILLISECONDS)
+                          .setConnectTimeout(connectTimeout, TimeUnit.MILLISECONDS)
+                          .setResponseTimeout(connectTimeout * 10, TimeUnit.MILLISECONDS);
 
-                    long linger = 420;
-                    SocketConfig.custom()
-                        .setSoTimeout(1000, TimeUnit.MILLISECONDS)
-                        .setSoLinger((int) linger, TimeUnit.MILLISECONDS);
-                }
-            }
-            """)
+                      long linger = 420;
+                      SocketConfig.custom()
+                          .setSoTimeout(1000, TimeUnit.MILLISECONDS)
+                          .setSoLinger((int) linger, TimeUnit.MILLISECONDS);
+                  }
+              }
+              """
+          )
         );
     }
 
@@ -170,53 +174,54 @@ class UpgradeApacheHttpClient5Test implements RewriteTest {
         rewriteRun(
           //language=java
           java(
-                """
-            import org.apache.http.HttpStatus;
-            import org.apache.http.client.methods.CloseableHttpResponse;
-            import org.apache.http.client.methods.HttpGet;
-            import org.apache.http.impl.client.CloseableHttpClient;
-            import org.apache.http.impl.client.HttpClientBuilder;
-            import org.apache.http.ProtocolVersion;
-
-            import java.io.IOException;
-
-            class A {
-                void method() throws IOException {
-                    HttpGet httpGet = new HttpGet("https://moderne.io");
-                    CloseableHttpClient instance = HttpClientBuilder.create().build();
-                    CloseableHttpResponse response = instance.execute(httpGet);
-
-                    System.out.println("response.getStatusLine() :: " + response.getStatusLine());
-                    int statusCode = response.getStatusLine().getStatusCode();
-                    String reason = response.getStatusLine().getReasonPhrase();
-                    ProtocolVersion version = response.getStatusLine().getProtocolVersion();
-                }
-            }
-            """,
             """
-            import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-            import org.apache.hc.core5.http.HttpStatus;
-            import org.apache.hc.client5.http.classic.methods.HttpGet;
-            import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-            import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-            import org.apache.hc.core5.http.ProtocolVersion;
-            import org.apache.hc.core5.http.message.StatusLine;
+              import org.apache.http.HttpStatus;
+              import org.apache.http.client.methods.CloseableHttpResponse;
+              import org.apache.http.client.methods.HttpGet;
+              import org.apache.http.impl.client.CloseableHttpClient;
+              import org.apache.http.impl.client.HttpClientBuilder;
+              import org.apache.http.ProtocolVersion;
 
-            import java.io.IOException;
+              import java.io.IOException;
 
-            class A {
-                void method() throws IOException {
-                    HttpGet httpGet = new HttpGet("https://moderne.io");
-                    CloseableHttpClient instance = HttpClientBuilder.create().build();
-                    CloseableHttpResponse response = instance.execute(httpGet);
+              class A {
+                  void method() throws IOException {
+                      HttpGet httpGet = new HttpGet("https://moderne.io");
+                      CloseableHttpClient instance = HttpClientBuilder.create().build();
+                      CloseableHttpResponse response = instance.execute(httpGet);
 
-                    System.out.println("response.getStatusLine() :: " + new StatusLine(response));
-                    int statusCode = response.getCode();
-                    String reason = response.getReasonPhrase();
-                    ProtocolVersion version = response.getVersion();
-                }
-            }
-            """)
+                      System.out.println("response.getStatusLine() :: " + response.getStatusLine());
+                      int statusCode = response.getStatusLine().getStatusCode();
+                      String reason = response.getStatusLine().getReasonPhrase();
+                      ProtocolVersion version = response.getStatusLine().getProtocolVersion();
+                  }
+              }
+              """,
+            """
+              import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+              import org.apache.hc.core5.http.HttpStatus;
+              import org.apache.hc.client5.http.classic.methods.HttpGet;
+              import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+              import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+              import org.apache.hc.core5.http.ProtocolVersion;
+              import org.apache.hc.core5.http.message.StatusLine;
+
+              import java.io.IOException;
+
+              class A {
+                  void method() throws IOException {
+                      HttpGet httpGet = new HttpGet("https://moderne.io");
+                      CloseableHttpClient instance = HttpClientBuilder.create().build();
+                      CloseableHttpResponse response = instance.execute(httpGet);
+
+                      System.out.println("response.getStatusLine() :: " + new StatusLine(response));
+                      int statusCode = response.getCode();
+                      String reason = response.getReasonPhrase();
+                      ProtocolVersion version = response.getVersion();
+                  }
+              }
+              """
+          )
         );
     }
 
@@ -226,41 +231,69 @@ class UpgradeApacheHttpClient5Test implements RewriteTest {
           //language=java
           java(
             """
-        import org.apache.http.client.methods.CloseableHttpResponse;
-        import org.apache.http.client.methods.HttpUriRequest;
-        import org.apache.http.impl.client.CloseableHttpClient;
-        import org.apache.http.impl.client.HttpClientBuilder;
-        import org.apache.http.client.methods.RequestBuilder;
+              import org.apache.http.client.methods.CloseableHttpResponse;
+              import org.apache.http.client.methods.HttpUriRequest;
+              import org.apache.http.impl.client.CloseableHttpClient;
+              import org.apache.http.impl.client.HttpClientBuilder;
+              import org.apache.http.client.methods.RequestBuilder;
 
-        import java.io.IOException;
+              import java.io.IOException;
 
-        class A {
-            void method() throws IOException {
-                RequestBuilder requestBuilder = RequestBuilder.get("https://moderne.io");
-                HttpUriRequest request = requestBuilder.build();
-                CloseableHttpClient instance = HttpClientBuilder.create().build();
-                CloseableHttpResponse response = instance.execute(request);
-            }
-        }
-        """,
-        """
-        import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
-        import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-        import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-        import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-        import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
+              class A {
+                  void method() throws IOException {
+                      RequestBuilder requestBuilder = RequestBuilder.get("https://moderne.io");
+                      HttpUriRequest request = requestBuilder.build();
+                      CloseableHttpClient instance = HttpClientBuilder.create().build();
+                      CloseableHttpResponse response = instance.execute(request);
+                  }
+              }
+              """,
+            """
+              import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+              import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+              import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+              import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+              import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 
-        import java.io.IOException;
+              import java.io.IOException;
 
-        class A {
-            void method() throws IOException {
-                ClassicRequestBuilder requestBuilder = ClassicRequestBuilder.get("https://moderne.io");
-                HttpUriRequest request = requestBuilder.build();
-                CloseableHttpClient instance = HttpClientBuilder.create().build();
-                CloseableHttpResponse response = instance.execute(request);
-            }
-        }
-        """)
+              class A {
+                  void method() throws IOException {
+                      ClassicRequestBuilder requestBuilder = ClassicRequestBuilder.get("https://moderne.io");
+                      HttpUriRequest request = requestBuilder.build();
+                      CloseableHttpClient instance = HttpClientBuilder.create().build();
+                      CloseableHttpResponse response = instance.execute(request);
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void changeUsernamePasswordCredentials() {
+        rewriteRun(
+          //language=java
+          java(
+            """
+              import org.apache.http.auth.UsernamePasswordCredentials;
+
+              class Example {
+                  void method() {
+                      UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("username", "password");
+                  }
+              }
+              """,
+            """
+              import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
+
+              class Example {
+                  void method() {
+                      UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("username", "password".toCharArray());
+                  }
+              }
+              """
+          )
         );
     }
 }
