14db14e35cc92b3dd3b266ea5513c3570bdaa672
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2025-04-15 10:14:05 +0100

    Migrate `org.apache.commons.lang3.CharEncoding` to StandardCharsets (#61)
    * feat: replace usages of `org.apache.commons.lang3.CharEncoding#CHAR_SET` with the corresponding `java.nio.charset.StandardCharsets#CHAR_SET.name()`
    
    * Add License
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Minor polish
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

---
+++ src/test/java/org/openrewrite/apache/commons/lang/ApacheCommonsLang3UseStandardCharsetsTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.apache.commons.lang;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.config.Environment;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class ApacheCommonsLang3UseStandardCharsetsTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.parser(JavaParser.fromJavaVersion().classpath("commons-lang3"))
+          .recipeFromResource("/META-INF/rewrite/apache-commons-lang-3.yml","org.openrewrite.apache.commons.lang3.UseStandardCharsets");
+    }
+
+    @SuppressWarnings({"UnusedAssignment", "deprecation"})
+    @Test
+    @DocumentExample
+    void convertCharEncodingFieldsToJdk() {
+        // language=java
+        rewriteRun(
+          java(
+            """
+              import org.apache.commons.lang3.CharEncoding;
+
+              class A {
+                  String test() {
+                      String encoding = CharEncoding.ISO_8859_1;
+                      encoding = CharEncoding.US_ASCII;
+                      encoding = CharEncoding.UTF_16;
+                      encoding = CharEncoding.UTF_16BE;
+                      encoding = CharEncoding.UTF_16LE;
+                      encoding = CharEncoding.UTF_8;
+                      return encoding;
+                  }
+              }
+              """,
+            """
+              import java.nio.charset.StandardCharsets;
+
+              class A {
+                  String test() {
+                      String encoding = StandardCharsets.ISO_8859_1.name();
+                      encoding = StandardCharsets.US_ASCII.name();
+                      encoding = StandardCharsets.UTF_16.name();
+                      encoding = StandardCharsets.UTF_16BE.name();
+                      encoding = StandardCharsets.UTF_16LE.name();
+                      encoding = StandardCharsets.UTF_8.name();
+                      return encoding;
+                  }
+              }
+              """
+          )
+        );
+    }
+}
