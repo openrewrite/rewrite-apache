be2c5394f78ae166187736073fe0e6debda5ce45
Pierre Delagrave <pierre@moderne.io>
2025-07-02 10:09:31 -0400

    Declarative only

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
 
     implementation("commons-io:commons-io:2.+")
     implementation("org.apache.commons:commons-lang3:3.+")
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.5+")
     implementation("org.apache.maven.shared:maven-shared-utils:3.+")
     implementation("org.codehaus.plexus:plexus-utils:3.+")
 
