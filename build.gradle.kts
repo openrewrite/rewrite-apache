1b3224b6558cad779f87ac73a345d58a8f059706
Tim te Beek <timtebeek@gmail.com>
2025-07-05 17:36:56 +0000

    Avoid `-SNAPSHOT` versions of JUnit 5.+
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.text.FindAndReplace?organizationId=ODQ2MGExMTUtNDg0My00N2EwLTgzMGMtNGE1NGExMTBmZDkw#defaults%3DW3sibmFtZSI6ImZpbmQiLCJ2YWx1ZSI6Imp1bml0LWp1cGl0ZXItZW5naW5lOjUuKyJ9LHsibmFtZSI6InJlcGxhY2UiLCJ2YWx1ZSI6Imp1bml0LWp1cGl0ZXItZW5naW5lOjUuMTMuMyJ9LHsibmFtZSI6ImZpbGVQYXR0ZXJuIiwidmFsdWUiOiIqKi9idWlsZC5ncmFkbGUua3RzIn1d
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,6 @@ dependencies {
 
     testRuntimeOnly("org.codehaus.plexus:plexus-container-default:2.+")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.+")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.13.3")
 
 }
