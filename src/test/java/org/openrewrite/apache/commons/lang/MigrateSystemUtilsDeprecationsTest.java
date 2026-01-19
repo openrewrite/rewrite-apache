7302cd85bbf2ae9de8d7e2272f46e9f2cefe30c8
Tim te Beek <tim@moderne.io>
2026-01-19 14:25:25 +0100

    Apply formatter to tests

--- src/test/java/org/openrewrite/apache/commons/lang/MigrateSystemUtilsDeprecationsTest.java
+++ src/test/java/org/openrewrite/apache/commons/lang/MigrateSystemUtilsDeprecationsTest.java
@@ -28,114 +28,114 @@ class MigrateSystemUtilsDeprecationsTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec
-                .parser(JavaParser.fromJavaVersion().classpath("commons-lang3"))
-                .recipeFromResources("org.openrewrite.apache.commons.lang3.MigrateSystemUtilsDeprecations");
+          .parser(JavaParser.fromJavaVersion().classpath("commons-lang3"))
+          .recipeFromResources("org.openrewrite.apache.commons.lang3.MigrateSystemUtilsDeprecations");
     }
 
     @DocumentExample
     @Test
     void fileSeparator() {
         rewriteRun(
-                java(
-                        """
-                                import org.apache.commons.lang3.SystemUtils;
+          java(
+            """
+              import org.apache.commons.lang3.SystemUtils;
 
-                                class Test {
-                                    String s = SystemUtils.FILE_SEPARATOR;
-                                }
-                                """,
-                        """
-                                import java.io.File;
+              class Test {
+                  String s = SystemUtils.FILE_SEPARATOR;
+              }
+              """,
+            """
+              import java.io.File;
 
-                                class Test {
-                                    String s = File.separator;
-                                }
-                                """));
+              class Test {
+                  String s = File.separator;
+              }
+              """));
     }
 
     @Test
     void pathSeparator() {
         rewriteRun(
-                java(
-                        """
-                                import org.apache.commons.lang3.SystemUtils;
+          java(
+            """
+              import org.apache.commons.lang3.SystemUtils;
 
-                                class Test {
-                                    String s = SystemUtils.PATH_SEPARATOR;
-                                }
-                                """,
-                        """
-                                import java.io.File;
+              class Test {
+                  String s = SystemUtils.PATH_SEPARATOR;
+              }
+              """,
+            """
+              import java.io.File;
 
-                                class Test {
-                                    String s = File.pathSeparator;
-                                }
-                                """));
+              class Test {
+                  String s = File.pathSeparator;
+              }
+              """));
     }
 
     @Test
     void lineSeparator() {
         rewriteRun(
-                java(
-                        """
-                                import org.apache.commons.lang3.SystemUtils;
+          java(
+            """
+              import org.apache.commons.lang3.SystemUtils;
 
-                                class Test {
-                                    String s = SystemUtils.LINE_SEPARATOR;
-                                }
-                                """,
-                        """
-                                class Test {
-                                    String s = System.lineSeparator();
-                                }
-                                """));
+              class Test {
+                  String s = SystemUtils.LINE_SEPARATOR;
+              }
+              """,
+            """
+              class Test {
+                  String s = System.lineSeparator();
+              }
+              """));
     }
 
     @Test
     void isJava19() {
         rewriteRun(
-                java(
-                        """
-                                import org.apache.commons.lang3.SystemUtils;
+          java(
+            """
+              import org.apache.commons.lang3.SystemUtils;
 
-                                class Test {
-                                    boolean b = SystemUtils.IS_JAVA_1_9;
-                                }
-                                """,
-                        """
-                                import org.apache.commons.lang3.SystemUtils;
+              class Test {
+                  boolean b = SystemUtils.IS_JAVA_1_9;
+              }
+              """,
+            """
+              import org.apache.commons.lang3.SystemUtils;
 
-                                class Test {
-                                    boolean b = SystemUtils.IS_JAVA_9;
-                                }
-                                """));
+              class Test {
+                  boolean b = SystemUtils.IS_JAVA_9;
+              }
+              """));
     }
 
     @Test
     void replaceKeysWithSystemProperties() {
         rewriteRun(
-                java(
-                        """
-                                import org.apache.commons.lang3.SystemUtils;
+          java(
+            """
+              import org.apache.commons.lang3.SystemUtils;
 
-                                class Test {
-                                    String k1 = SystemUtils.USER_HOME_KEY;
-                                    String k2 = SystemUtils.USER_NAME_KEY;
-                                    String k3 = SystemUtils.USER_DIR_KEY;
-                                    String k4 = SystemUtils.JAVA_IO_TMPDIR_KEY;
-                                    String k5 = SystemUtils.JAVA_HOME_KEY;
-                                }
-                                """,
-                        """
-                                import org.apache.commons.lang3.SystemProperties;
+              class Test {
+                  String k1 = SystemUtils.USER_HOME_KEY;
+                  String k2 = SystemUtils.USER_NAME_KEY;
+                  String k3 = SystemUtils.USER_DIR_KEY;
+                  String k4 = SystemUtils.JAVA_IO_TMPDIR_KEY;
+                  String k5 = SystemUtils.JAVA_HOME_KEY;
+              }
+              """,
+            """
+              import org.apache.commons.lang3.SystemProperties;
 
-                                class Test {
-                                    String k1 = SystemProperties.USER_HOME;
-                                    String k2 = SystemProperties.USER_NAME;
-                                    String k3 = SystemProperties.USER_DIR;
-                                    String k4 = SystemProperties.JAVA_IO_TMPDIR;
-                                    String k5 = SystemProperties.JAVA_HOME;
-                                }
-                                """));
+              class Test {
+                  String k1 = SystemProperties.USER_HOME;
+                  String k2 = SystemProperties.USER_NAME;
+                  String k3 = SystemProperties.USER_DIR;
+                  String k4 = SystemProperties.JAVA_IO_TMPDIR;
+                  String k5 = SystemProperties.JAVA_HOME;
+              }
+              """));
     }
 }
